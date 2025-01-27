3507575946bd85f0e31a59c73c1d1ec9bae06bb9
athervisingh <singhathervi001@gmail.com>
2025-01-28 00:49:11 +0530

    courseDetails

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Admin from "./route/admin/admin.compoent";
 import Navigation from "./route/navigation/navigation.component";
 import Home from "./route/coursePortal/coursePortal.component";
+import CourseDetail from "./route/courseDetail/courseDetail.component"; // Import the dynamic course detail component
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/admin" element={<Admin />} />
+        {/* Dynamic route for course details */}
+        <Route path="/course/:title" element={<CourseDetail />} />
       </Route>
     </Routes>
   );
